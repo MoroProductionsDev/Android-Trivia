e293364bcf88c204656dae150bd5da9ab7db90b9
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-11-15 01:33:47 -0800

    - Added linkage in the fragment resource. Title to game  fragment view - Added Kotlin code to TitleFragment that add event to   playbutton to change from title fragment to the game   fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import com.example.android.navigation.databinding.FragmentTitleBinding
 import java.util.zip.Inflater
 
@@ -20,6 +21,10 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
             val binding =  DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,
                                                         container, false)
+        // THis sets on event that whenever the play button is clicked, it goes fro the current view
+        // which is title framgent to the game fragment view.
+        binding.playButton.setOnClickListener{ view : View ->
+               Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)       }
         return binding.root
     }
 }
