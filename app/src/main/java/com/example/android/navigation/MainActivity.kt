350d5772d84da427e6e99feeb3dd5926c56c7de7
Raul Rivero Rubio <51531978+MoroProductionsDev@users.noreply.github.com>
2019-11-15 20:56:14 -0800

    - added locked and unlocked to nav header.   Locked when the trivia starts.   Unlocked in the menu.s

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -20,6 +20,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -40,6 +42,16 @@ class MainActivity : AppCompatActivity() {
         //
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
         NavigationUI.setupWithNavController(binding.idNavView, navController)
+
+        // This will lock the nav header bar from been unlocked when the game start.
+        // I'll be lock when the game start, unlock when the menu
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
